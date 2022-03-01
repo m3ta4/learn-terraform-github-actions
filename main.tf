192b2defc74101969e8ca75dbeeb9fc3acf0eba6
Kevin Stuart <kevin@conexed.com>
2022-03-01 12:44:16 -0700

    Updated ami query owner.

--- main.tf
+++ main.tf
@@ -29,6 +29,7 @@ provider "aws" {
 provider "random" {}
 
 data "aws_ami" "us-west-1" {
+  owners = ["099720109477"]
   most_recent = true
 
   filter {
@@ -48,6 +49,7 @@ data "aws_ami" "us-west-1" {
 }
 
 data "aws_ami" "us-west-2" {
+  owners = ["099720109477"]
   most_recent = true
 
   filter {
